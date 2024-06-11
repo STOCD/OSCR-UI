422aee6dd2d76bd2fda6961bce87d3117579c685
Kraust <secretdragoon@gmail.com>
2024-06-11 07:40:09 -0400

    Use cx_Freeze instead of Nuitka for Distributing OSCR. (#39)
    * Added Combat Time to Copy Summary.
    
    * Updated league connector for new backend.
    
    * Update League Connector to use oscr.stobuilds.com (HTTP)
    
    * Updated oscr.stobilds.com to use HTTPS
    
    * Updated README
    
    * Reworded
    
    * Removed word
    
    * Updated README again
    
    * Updated to also reference CLA.
    
    * Moved section down
    
    * Updated Error message on log failure.
    
    * Updated messages again.
    
    * Updated pyproject.toml
    
    * Updated error log reporting in league connector.
    
    * And removed debug print
    
    * Updated version
    
    * Updated OSCR client, Enabled New Upload Result Dialog
    
    * Updated Version
    
    * Testing something.
    
    * Testing something.
    
    * Try something else
    
    * Having fun yet?
    
    * Bootstrap venv
    
    * PS1
    
    * bat
    
    * idk I'm tired
    
    * Woops
    
    * Add executable
    
    * Trying to use cx_Freeze
    
    * Trying to get cx_Freeze working.
    
    * Test Build
    
    * czfreeze
    
    * Test?
    
    * More
    
    * More testing
    
    * idk
    
    * ZIP Exclude Packages
    
    * bdis_msi
    
    * Try and include assets
    
    * idk kinda stuck
    
    * Check if frozen
    
    * Compilation error
    
    * Icon and Shortcut?
    
    * Was using the wrong syntax
    
    * Another typo
    
    * Try and add more to the msi
    
    * Typo
    
    * Try this instead
    
    * toml is very strange
    
    * Doesn't like that ICON
    
    * ico is not optional
    
    * Good enough for now
    
    * Only create MSI on release
    
    * Bumped up version
    
    * Remove OSCR_settings
    
    * Updated build workflow
    
    * Update to include README and LICENSE

--- shell.nix
+++
@@ -1,9 +0,0 @@
-{ nixpkgs ? import <nixpkgs> {} }:
-
-nixpkgs.mkShell {
-  nativeBuildInputs = with nixpkgs; [
-    nixpkgs.python311
-    nixpkgs.python311Packages.pyside6
-    nixpkgs.qt6.full
-  ];
-}
