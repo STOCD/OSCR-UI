5dd2976e03a8dbb596b7d47633d277b2ef70e9b5
Shinga13 <93780215+Shinga13@users.noreply.github.com>
2026-03-21 21:38:59 +0100

    cleaning up iofunctions.py

--- OSCRUI/iofunctions.py
+++ OSCRUI/iofunctions.py
@@ -6,10 +6,6 @@ import webbrowser
 from PySide6.QtWidgets import QFileDialog
 from PySide6.QtGui import QIcon
 
-# --------------------------------------------------------------------------------------------------
-# object methods
-# --------------------------------------------------------------------------------------------------
-
 
 def browse_path(
         preset_path: Path, types: str = 'Any File (*.*)', save: bool = False) -> Path | None:
@@ -40,10 +36,6 @@ def browse_path(
             return selected_path
         else:
             return None
-
-# --------------------------------------------------------------------------------------------------
-# static functions
-# --------------------------------------------------------------------------------------------------
 
 
 def get_asset_path(asset_name: str, app_directory: str) -> str:
@@ -95,55 +87,6 @@ def open_link(link: str = ''):
     """
     if link:
         webbrowser.open(link, new=2, autoraise=True)
-
-
-def sanitize_file_name(txt, chr_set='extended') -> str:
-    """Converts txt to a valid filename.
-
-    Parameters:
-    - :param txt: The path to convert.
-    - :param chr_set:
-        - 'printable':    Any printable character except those disallowed on Windows/*nix.
-        - 'extended':     'printable' + extended ASCII character codes 128-255
-        - 'universal':    For almost *any* file system.
-    """
-    FILLER = '-'
-    MAX_LEN = 255  # Maximum length of filename is 255 bytes in Windows and some *nix flavors.
-
-    # Step 1: Remove excluded characters.
-    BLACK_LIST = set(chr(127) + r'<>:"/\|?*')
-    white_lists = {
-        'universal': {'-.0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'},
-        'printable': {chr(x) for x in range(32, 127)} - BLACK_LIST,     # 0-32, 127 are unprintable,
-        'extended': {chr(x) for x in range(32, 256)} - BLACK_LIST,
-    }
-    white_list = white_lists[chr_set]
-    result = ''.join(x if x in white_list else FILLER for x in txt)
-
-    # Step 2: Device names, '.', and '..' are invalid filenames in Windows.
-    DEVICE_NAMES = (
-            'CON', 'PRN', 'AUX', 'NUL', 'COM1', 'COM2', 'COM3', 'COM4', 'COM5', 'COM6', 'COM7',
-            'COM8', 'COM9', 'LPT1', 'LPT2', 'LPT3', 'LPT4', 'LPT5', 'LPT6', 'LPT7', 'LPT8', 'LPT9',
-            'CONIN$', 'CONOUT$', '..', '.')
-    if '.' in txt:
-        name, _, ext = result.rpartition('.')
-        ext = f'.{ext}'
-    else:
-        name = result
-        ext = ''
-    if name in DEVICE_NAMES:
-        result = f'-{result}-{ext}'
-
-    # Step 3: Truncate long files while preserving the file extension.
-    if len(result) > MAX_LEN:
-        result = result[:MAX_LEN - len(ext)] + ext
-
-    # Step 4: Windows does not allow filenames to end with '.' or ' ' or begin with ' '.
-    result = result.strip()
-    while len(result) > 0 and result[-1] == '.':
-        result = result[:-1]
-
-    return result
 
 
 def save_to_json(path: Path, data: dict):
